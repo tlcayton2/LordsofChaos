afaceb8c6fef4ac59ff58bb3faf3040061ffbe3a
tlcayton2 <tlcayton2@gmail.com>
2015-03-10 13:27:36 -0700

    Disease the people

--- createNetwork.py
+++ createNetwork.py
@@ -8,7 +8,7 @@ import numpy as np
 import csv
 
 pop = 355 #student population
-slots = 22 #number of timeslots
+slots = 44 #number of timeslots
 
 #create adjacency array 
 filename = 'C:\Users\Tiawna\Documents\GitHub\LordsofChaos\\timeSchedule.csv'
@@ -16,31 +16,11 @@ crn_adjacency = np.genfromtxt(filename, delimiter = ',')
 #print crn_adjacency # student x timeslot
 
 # adjacency(i,j,k) = adjacency(st1,st2,ts)
-adjacency = np.zeros([pop,pop,slots])
+adjacency = np.zeros([pop,pop,slots+33])
 
 for k in range(slots):
     for j in range(pop):
         for i in range(pop):
-            if (int(crn_adjacency[i,k]) != 0 and crn_adjacency[i,k] == crn_adjacency[j,k]):
+            if (int(crn_adjacency[i,k%22]) != 0 and crn_adjacency[i,k%22] == crn_adjacency[j,k%22]):
                 adjacency[i,j,k] = 1
-
-beta = 1.3  #infection rate
-gamma = 1.    #recovery rate
-patient = 1 #first infected student
-id_infected = np.zeros([pop,1]) #x in SIS model function
-id_infected[patient] = 1
-time = 0
-
-#chi(t) = beta(I-diag(x))Ax-gamma*x
-#I = identity
-#x = id_infected
-def sis_model(adjacency, beta, gamma, time, id_infected):
-    identity = np.eye(pop)
-    diag_x = np.diag(id_infected)
-    current_adjacency = np.matrix(adjacency[:,:,time])
-    chi = beta*(identity - diag_x)*current_adjacency*id_infected - gamma*id_infected
-    return chi
-    
-snapshot = sis_model(adjacency, beta, gamma, time, id_infected)
-
 
---
+++ tempSis.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 09 16:06:19 2015
+
+@author: Tiawna
+"""
+
+import random as rd
+import math
+from operator import add
+
+beta = .5
+kappa = .35
+patient = 1
+id_infected = np.zeros(pop)
+id_infected[patient] = 1
+
+#current_adjacency = adjacency[:,:,time]
+
+
+def sis_model(current_adjacency, id_infected, beta, kappa):
+    sick_student = np.zeros(355)    
+    for i, student in enumerate(id_infected):
+        
+        if student > 0:
+            id_infected[i] += 1
+            index = np.where(current_adjacency[:,i] == 1)[0]
+            index = index.astype(int)
+            l = len(index)
+            new = int(math.ceil(l*kappa))
+            contact = rd.sample(index,new)
+            for item in contact:
+                x = rd.random()
+                if x < beta and id_infected[item] == 0:
+                    sick_student[item] = 1
+                    
+        temp_infected = map(add, id_infected, sick_student)
+    return temp_infected
+        
+#snapshot = sis_model(current_adjacency, id_infected, beta, kappa)
+
+def disease_spread(start, stop, adjacency, id_infected):
+#    infection_array = np.zeros([pop, (stop - start+1)])    
+    infection_array = np.zeros([pop,(stop - start + 2)])    
+    infection_array[:,0] = id_infected
+    j = 1
+    for time in range(start, stop+1):
+        current_adjacency = adjacency[:,:,time]
+        current_id = np.zeros([pop,1])
+        current_id = sis_model(current_adjacency, infection_array[:,j-1], beta, kappa )
+#        infection_array = np.insert(infection_array, current_id,,1)
+        infection_array[:,j] = healthy(current_id)
+        j +=1  
+    return infection_array
+    
+def healthy(id_infected):
+    for i,student in enumerate(id_infected):
+        if student > 44:
+            id_infected[i] = 0
+    return id_infected
+    
+snapshot = disease_spread(1,76, adjacency, id_infected)
