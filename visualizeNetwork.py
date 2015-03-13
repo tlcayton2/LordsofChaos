f9c2ee95ea150c265685560775fda19206c2e9fd
atomson <Ayme@169-231-102-29.wireless.ucsb.edu>
2015-03-13 16:44:29 -0700

    Working on visualization

--- visualizeNetwork.py
+++ visualizeNetwork.py
@@ -6,8 +6,17 @@ Created on Mon Mar  9 20:39:26 2015
 """
 import networkx as nx
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
 import random as rd
+import tkinter as Tk
+
+root = Tk.Tk()
+root.wm_title("Resident Evil Paradigm")
 
 
 adjacency = np.zeros([10, 10])
@@ -27,6 +36,12 @@ snapshot = [ rd.randint(0,4) for i in range(10)]
 
 def showSpread(adjacency, snapshot):
 
+        
+    
+    
+    
+    
+    
     G = nx.from_numpy_matrix(adjacency)
 
     healthColor = {0: 0.6,
@@ -36,11 +51,21 @@ def showSpread(adjacency, snapshot):
                    4: 1.0}
 
     hColors = [healthColor.get(student) for student in snapshot]
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, node_color=hColors)
 
-    nx.draw(G, node_color=hColors)
-
-    plt.show()
+    f = Figure(figsize=(5,4), dpi=100)
     
 
+    canvas = FigureCanvasTkAgg(plt.figure(1), master=root)
+    toolbar = NavigationToolbar2TkAgg( canvas, root )
+    toolbar.update()
+
+    canvas.show()
+    canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+    canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+    
+    Tk.mainloop()
+    
 showSpread(adjacency, snapshot)
 
