c6eef01b2d75cb35d9cb1a2f0a0321596ce04c38
tlcayton2 <tlcayton2@gmail.com>
2015-03-16 17:07:28 -0700

    Powerpoint and updates

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 import Tkinter as tk
 
 # ----------------------------- SIS Model Globals ---------------------------
-beta = .5                           # Infection rate
+beta = .05                           # Infection rate
 kappa = .35                         # Mixing parameter
 start = 0
 stop = 10*11*7
@@ -32,8 +32,8 @@ try:
     adjacency
 except NameError:
     #create adjacency array 
-    #filename = 'C:\Users\Tiawna\Documents\GitHub\LordsofChaos\\timeSchedule.csv'
-    filename = 'timeSchedule.csv'
+    filename = 'C:\Users\Tiawna\Documents\GitHub\LordsofChaos\\timeSchedule.csv'
+    #filename = 'timeSchedule.csv'
     crn_adjacency = np.genfromtxt(filename, delimiter = ',')
     #print crn_adjacency # student x timeslot
 
@@ -46,11 +46,16 @@ except NameError:
                 if (int(crn_adjacency[i,k%22]) != 0 and crn_adjacency[i,k%22] == crn_adjacency[j,k%22]):
                     adjacency[i,j,k] = 1
 
+    masterAdjacency = adjacency.sum(2)
+    
     schedFile = 'schedules.csv'
     sched = csv.reader(open(schedFile,'r'))
     StuSchedDict = {}
     for row in sched:
         StuSchedDict[row[0]] = row[1]
+        
+masterAdjacency = adjacency.sum(2)
+
 # ============================================================================
 # |                           Function Definitions                           |
 # ============================================================================
@@ -95,8 +100,8 @@ def disease_spread(start, stop, adjacency, id_infected):   # fcn to create array
     infection_array = np.zeros([pop,(stop - start + 2)])   
     infection_array[:,0] = id_infected
     j = 1
-    for time in range(start, stop+1):
-        current_adjacency = adjacency[:,:,time]
+    for time in range(start, stop):
+        current_adjacency = adjacency[:,:,time%77]
         current_id = np.zeros([pop,1])
         infected = infection_array[:,j-1]
         current_id = sis_model(current_adjacency, infected, beta, kappa )
@@ -107,7 +112,7 @@ def disease_spread(start, stop, adjacency, id_infected):   # fcn to create array
             break
     return infection_array
 # ----------------------------------------------------------------------------
-# (4.) Sick Count
+# (4.) Batch Model Sick Count
 # ----------------------------------------------------------------------------
 def sickCount(infection_array):                   # function to return the # of 
     timeslot_sum = (infection_array!=0).sum(0)    # infected individuals at the
@@ -118,7 +123,7 @@ def sickCount(infection_array):                   # function to return the # of
             sickperday[i] = timeslot_sum[x]
             i += 1
             print x
-    return sickperday
+    return sickperday/nSimulations
 # ----------------------------------------------------------------------------
 # (5.) SIS Model
 # ----------------------------------------------------------------------------
@@ -138,14 +143,15 @@ def AnalySIS(adjacency, nSimulations):
 # (6.) GUI SIS Model
 # ----------------------------------------------------------------------------
 def GUIAnalySIS(adjacency, patient0, start, stop):
-    effectOfSpread = np.zeros([1, 77])
     id_infected = np.zeros(355)
     id_infected[patient0] = 1       
     infection_array = disease_spread(start, stop, adjacency, id_infected)
-    effectOfSpread += sickCount(infection_array)
-    return (infection_array, effectOfSpread)
+    #effectOfSpread = singleSickCount(infection_array)
+    #return (infection_array, effectOfSpread)
+    return infection_array
+    
 # ----------------------------------------------------------------------------
-# (7.) GUI Class
+# (8.) GUI Class
 # ----------------------------------------------------------------------------
 
 class MainWindow(tk.Frame):
@@ -166,16 +172,16 @@ class MainWindow(tk.Frame):
         self.button = tk.Button(self, text="Run Simulation", 
                                 command=self.simulation)
         self.button.pack()
-        def quit(self):
-            self.destroy()
-        tk.Button(self, text="Quit", command=lambda self=self:quit(self)).pack()
-
+        
 
     def simulation(self):
         perm = self.entrytext1.get()
         day = self.entrytext2.get()
   
-        infection_array, effectOfSpread = GUIAnalySIS(adjacency, int(perm), int(day), stop)
+        infection_array = GUIAnalySIS(adjacency, int(perm), int(day), stop)
+        patientCount = max((infection_array!=0).sum(0))
+        mostSick = np.argmax((infection_array!=0).sum(0))
+
         self.counter += 1
         t = tk.Toplevel(self)
         t.wm_title("WHAT HAPPENS WHEN YOU GO TO CLASS SICK?")
@@ -183,14 +189,14 @@ class MainWindow(tk.Frame):
         l1.pack()
         l5 = tk.Label(t, text="Student Perm: "+day)
         l5.pack()
-        l2 = tk.Label(t, text="Major: "+ "chemE")
+        l2 = tk.Label(t, text="Major: "+ StuSchedDict[perm])
         l2.pack()
-        l3 = tk.Label(t, text="Number sick from first class attended: "+ "10")
+        l3 = tk.Label(t, text="Peak infection time:"+ str(mostSick))
         l3.pack()
-        l4 = tk.Label(t, text="Total number sick in quarter: "+"45")
+        l4 = tk.Label(t, text="Number of people infected at peak: "+ str(patientCount) )
         l4.pack()
 
-        G = nx.from_numpy_matrix(adjacency[:,:,start])
+        G = nx.from_numpy_matrix(masterAdjacency)
 #        G.node[perm]['shape']='square'
 
 #        nodeList = np.arange(10)
@@ -198,7 +204,7 @@ class MainWindow(tk.Frame):
 #        nodeShape[perm] = '*'
 
         pos = nx.spring_layout(G)       
-        nx.draw(G, pos, node_color=infection_array[:,start]/44.0, cmap = plt.get_cmap('YlOrBr'))
+        nx.draw(G, node_color=infection_array[:,start]/44.0, cmap = plt.get_cmap('YlOrBr'))
         #nx.draw(G, pos, nodelist=nodeList, node_shape=nodeShape, node_color=range(10), cmap = plt.get_cmap('YlOrBr'))          
         
         canvas = FigureCanvasTkAgg(plt.figure(1), master=t)
@@ -206,11 +212,60 @@ class MainWindow(tk.Frame):
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
+    
+# ----------------------------------------------------------------------------
+# (9.) Dynamic Visualization
+# ----------------------------------------------------------------------------
+def presentationGUI():
+    patient0 = input('What is your perm?')
+    start = 0
+    stop = 770
+    i = 0
+    
+    root = tk.Tk()
+    root.wm_title("Dynamic Spread of Sickness")
+    root.wm_protocol('WM_DELETE_WINDOW', root.quit())
+
+    f = plt.figure()
+    a = f.add_subplot(111)
+    plt.axis('off')
+
+
+    # initial network
+    infection_array = GUIAnalySIS(adjacency, patient0, start, stop)
+    G = nx.from_numpy_matrix(adjacency[:,:,0])
+#    pos = nx.spring_layout(G)
+    nx.draw(G, node_color=infection_array[:,0], cmap = plt.get_cmap('YlOrBr'), ax=a)
+    xlim=a.get_xlim()
+    ylim=a.get_ylim()
+
+    # a tk.DrawingArea
+    canvas = FigureCanvasTkAgg(f, master=root)
+    canvas.show()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    
+    def next_graph(infection_array):
+        a.cla()
+        #np.delete(adjacency, 0, 2)
+        #infection_array = np.delete(infection_array, 0, 1)
+        G = nx.from_numpy_matrix(masterAdjacency)        
+        nx.draw(G, node_color=infection_array[:,0], cmap = plt.get_cmap('YlOrBr'), ax=a)
+        a.set_xlim(xlim)
+        a.set_ylim(ylim)
+        plt.axis('off')
+        canvas.draw()
+        
+    b = tk.Button(root, text="next",command=next_graph(infection_array=np.delete(infection_array,0,1)))
+    b.pack()
+
+    tk.mainloop()
+
+
 
 # ============================================================================
 # |                       Run Single or Batch Simulation                     |
 # ============================================================================
-option = input('Do you want to run a single (\'s\') or a batch (\'b\') simulation? ')
+option = raw_input('Do you want to run a single (s), batch (b), or presentation (p) simulation? ')
 if option=='s':
     if __name__ == "__main__":
         root = tk.Tk()
@@ -222,11 +277,13 @@ if option=='s':
         root.mainloop()            
 elif option=='b':
     nSimulations = input('Enter number of simulations to run per student: ')
-    effectOfSpread_file = input('Enter path to dave data to, e.g., \'/Desktop/data\':')    
+    effectOfSpread_file = raw_input('Enter path to save data to, e.g., /Desktop/data:')    
 
     effectOfSpread = AnalySIS(adjacency, nSimulations)
     print effectOfSpread
     np.savetxt(effectOfSpread_file,effectOfSpread)
+elif option =='p':
+    presentationGUI()
 else:
     print 'Unexpected input, try again.'
     execfile('main.py')
