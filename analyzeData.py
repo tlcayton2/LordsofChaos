1b6998177f5d99d89f9091f9cf3e3005c3ad065b
tlcayton2 <tlcayton2@gmail.com>
2015-03-16 23:00:03 -0700

    Small Batch Analysis

--- analyzeData.py
+++ analyzeData.py
@@ -8,15 +8,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-filename = 'C:\Users\Tiawna\Documents\GitHub\LordsofChaos\\effectOfSpread_nSim1000'
+filename = 'C:\Users\Tiawna\Documents\GitHub\LordsofChaos\\newFile'
 effectOfSpread = np.genfromtxt(filename, delimiter = ' ')
-effectOfSpread= np.around(effectOfSpread/1000)
+
+#%%
+
+maxCol = np.amax(effectOfSpread,axis = 0)
+maxRow = np.amax(effectOfSpread,axis = 1)
+
+plt.plot(maxCol)
+plt.ylabel('Max Number of Students Infected')
+plt.xlabel('Day')
+plt.title('Most Students Infected Per Day')
+plt.savefig('maxTime')
+plt.show()
+
+plt.plot(maxRow)
+plt.ylabel('Students Infected')
+plt.xlabel('Original Sick Student')
+plt.title('Who Infects the Most Students?')
+plt.savefig('maxStudent')
+plt.show()
+
+#%%
+
+avgCol = np.average(effectOfSpread, axis = 0)
+avgRow = np.average(effectOfSpread, axis = 1)
+
+plt.plot(avgCol)
+plt.ylabel('Number of Students Infected')
+plt.xlabel('Day')
+plt.title('Average Students Infected Per Day')
+plt.savefig('avgTime')
+plt.show()
+
+plt.plot(avgRow)
+plt.ylabel('Number of Students Infected')
+plt.xlabel('Original Sick Student')
+plt.title('Average Students Infected Over Quarter')
+plt.savefig('avgStudent')
+plt.show()
+
+
+
+#%%
 
 for t in range(355):
-    temp = effectOfSpread[t,1:10]
+    temp = effectOfSpread[t,:]
     plt.plot(temp)
     plt.show()
+    
 
-
-G = nx.from_numpy_matrix(masterAdjacency)
-draw_graphviz(G)
+    
